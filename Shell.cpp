6ae9e8d9ebed5efe8fb4d84ccba47666da4d358a
Pratik Parmar <prateek18597@gmail.com>
2019-02-14 02:13:28 +0530

    Shell Completed

--- Shell.cpp
+++ Shell.cpp
@@ -4,14 +4,45 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <limits.h>
 using namespace std;
+
+string getexepath()
+{
+  char result[ PATH_MAX ];
+  ssize_t count = readlink( "/proc/self/exe", result, PATH_MAX );
+  return std::string( result, (count > 0) ? count : 0 );
+}
 
 int main()
 {
-		string func,flag;
-		cout<<"->";
-		cin>>func;
-		cout<<"Prob";
+	while(true)
+	{
+		string program_name,parameters;
+		string path=getexepath();
+		int last=0;
+		for(int i=0;path[i];i++)
+		{
+			if(path[i]=='/')
+				last=i;
+		}
+		path=path.substr(0,last);
+		cout<<path<<">>>";
+		getline (cin, program_name);
+		if(program_name=="quit")
+			break;
+		bool flag=false;
+		for(int i=0;program_name[i];i++)
+		{
+			if(program_name[i]==' ')
+			{
+				flag=true;
+				parameters=program_name.substr(i+1,program_name.length()-i);
+				program_name=program_name.substr(0,i);
+				break;
+			}
+		}
+		
 		int rc = fork();
 		if (rc < 0) 
 		{
@@ -20,17 +51,22 @@ int main()
 		} 
 		else if (rc == 0) 
 		{
-			cout<<"Problem Here";
-			char *myargs[3];
-			myargs[0] = strdup(func);
-			myargs[1] = NULL;
-			myargs[2] = NULL;
-			execvp(myargs[0], myargs);
+			if(flag==false){
+				char *myargs[]={(char*)program_name.c_str(),NULL};
+				execvp(myargs[0], myargs);
+			}
+			else
+			{
+				char *myargs[]={(char*)program_name.c_str(),(char*)parameters.c_str(),NULL};
+				execvp(myargs[0], myargs);
+			}
+
+			
 		}
 		else 
 		{
 			int rc_wait = wait(NULL);
 		}
-	
+	}
 	return 0;
 }
